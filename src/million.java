10411412cf5f53c752fa9d5c03788df521e9c8b4
Andrei <ankorotkov66@gmail.com>
2019-08-11 13:27:26 +0400

    added package

--- src/million.java
+++ src/million.java
@@ -1,10 +1,12 @@
-import java.util.*;
+package com.epam.Andrei_Korotkov.java.lesson4.task2;
 
 /**
  * created by Andrei_Korotkov 8/9/2019
  */
-public class million {
 
+import java.util.*;
+
+public class million {
 
     public List<Integer> million;
 
@@ -12,65 +14,60 @@ public class million {
         this.million = million;
     }
 
-    public void addMillion () {
+    public void addMillion() {
         List<Integer> tmp = new ArrayList<Integer>();
-        for (int i = 0; i < 1000000; i++) {  // если оставить миллион, будет выполняться полторачаса+
+        for (int i = 0; i < 1000000; i++) {  // если оставить миллион, будет выполняться полторачаса+, лучше уменьшить
             tmp.add(i);
             Collections.shuffle(tmp);
         }
         setMillion(tmp);
     }
 
-    public void assertRandom () {
+    public void assertRandom() {
         for (int i = 0; i < 10; i++) {
             System.out.print(million.get(i) + ", ");
         }
     }
 
-    public void assertUnique () {
+    public void assertUnique() {
         boolean unique;
-        TreeSet <Integer> tmp = new TreeSet<>();
+        TreeSet<Integer> tmp = new TreeSet<>();
         tmp.addAll(million);
         if (tmp.size() == million.size()) {
             unique = true;
-        } else {unique = false;}
+        } else {
+            unique = false;
+        }
         System.out.println();
         System.out.println(unique);
     }
 
-    public void minElement () {
+    public void minElement() {
         System.out.println(Collections.min(million));
     }
 
-    public void removeOdd () {
-        million.removeIf(i->i%2!=0);
+    public void removeOdd() {
+        million.removeIf(i -> i % 2 != 0);
     }
 
-    public void nextToMaxElement () {
+    public void nextToMaxElement() {
         Collections.sort(million, new Comparator<Integer>() {
             public int compare(Integer i1, Integer i2) {
                 return i1.toString().compareTo(i2.toString());
             }
         });
-        System.out.println(million.get(million.size()-1));
+        System.out.println(million.get(million.size() - 2));
     }
-
 
     public static void main(String[] args) {
 
-        List<Integer> myArray = new ArrayList<Integer>();
-        for (int i = 0; i < 100; i++) {
-            myArray.add(i);
-            Collections.shuffle(myArray);
-        }
-
         million myMillion = new million();
+
         myMillion.addMillion();
 
         System.out.println(myMillion.million.size());
 
         myMillion.assertRandom();
-
 
         myMillion.assertUnique();
 
